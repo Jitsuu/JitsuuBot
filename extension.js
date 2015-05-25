32ef639c97e8e530a0b356b2b592d8d4ce343a31
Jitsuu <dkmedina2001@icloud.com>
2015-05-24 20:21:52 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -55,10 +55,10 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
+        botName: "JitsuuBot",
         language: "english",
-        startupCap: 1, // 1-200
-        startupVolume: 0, // 0-100
+        startupCap: 25, // 1-200
+        startupVolume: 17, // 0-100
         startupEmoji: false, // true or false
         cmdDeletion: true,
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
